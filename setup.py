033add39905867f01f548c57415054c6d75b3064
Fatih Karagöz <fatihkaragoz@192.168.1.117>
2025-03-01 14:48:39 +0300

    init the project to remote

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kagglehelper',
+    version='0.0.1',
+    description='a pip-installable package example',
+    license='MIT',
+    packages=find_packages(),
+    author='Fatih Karagoz',
+    author_email='fatihkkaragoz@gmail.com',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    keywords=['kaggle'],
+    install_requires=[
+       'kagglehub>=0.2.9'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+
+)
